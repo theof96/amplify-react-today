a3920d6cf403e53e4ccde8f2dc6a62af050ce21c
theof96 <theo.franquet@zeitmedical.com>
2022-10-05 23:18:46 +0200

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "todayAppStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
